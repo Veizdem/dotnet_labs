44a745651da005c4167b44a26753920eb0a7ee4d
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-01-05 23:17:37 +0200

    Пример 12. Вывод на консоль таблицы чисел с помощью форматирования строк

---
+++ lab_012/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "Таблица";
+            Console.Clear();
+
+            Console.WriteLine("Число Корень");
+
+            for (double i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0,4} {1,8:F4}", i, Math.Sqrt(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
