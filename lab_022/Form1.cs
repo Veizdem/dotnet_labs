4de52ca9719b0d5e621fb6798ecf29249f3d7d79
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-01-07 13:30:14 +0200

    Пример 22. Разрешаем вводить в текстовое поле цифры, а также разделитель целой и дробной части числа

---
+++ lab_022/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_022
+{
+    public partial class Form1 : Form
+    {
+        System.Globalization.CultureInfo cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+
+        string dotOrComa;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Введите число";
+
+            dotOrComa = cultureInfo.NumberFormat.NumberDecimalSeparator;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool isDotOrComaSelect = false;
+
+            if (char.IsDigit(e.KeyChar) == true) return;
+
+            if (e.KeyChar == (char)Keys.Back) return;
+
+            if (textBox1.Text.IndexOf(dotOrComa) != -1)
+            {
+                isDotOrComaSelect = true;
+            }
+
+            if (isDotOrComaSelect == true)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (e.KeyChar.ToString() == dotOrComa) return;
+
+            e.Handled = true;
+        }
+    }
+}
