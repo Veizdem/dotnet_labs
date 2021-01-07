f698fd5498b5a5384224c4e6782973a8e99e2885
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-01-07 11:51:04 +0200

    Пример 13. Вызов MessageBox.Show в консольном приложении. Формат даты и времени

---
+++ lab_013/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataTime = string.Format(
+                "(d) - это формат \"короткой\" даты: . . . . . . . . . {0:d}\n" +
+                "(D) - это формат \"полной\" даты: . . . . . . . . . . {0:D}\n" +
+                "(t) - это формат \"короткого\" времени: . . . . . . . {0:t}\n" +
+                "(T) - это формат \"длинного\" времени:. . . . . . . . {0:T}\n" +
+                "(f) - выводится \"полная\" дата и \"короткое\" время: {0:f}\n" +
+                "(F) - выводится \"полная\" дата и \"длинное\" время:. {0:F}\n" +
+                "(g) General - короткая дата и короткое время: . . . . {0:g}\n" +
+                "(G) General - \"общий\" формат: . . . . . . . . . . . {0:G}\n" +
+                "Пустой формат - такой же, как формат (G): . . . . . . {0}\n" +
+                "(M) - выводится только месяц и число: . . . . . . . . {0:M}\n" +
+                "(U) Universal full date/time - время по Гринвичу: . . {0:U}\n" +
+                "(Y) - по этому формату выводится только год:. . . . . {0:Y}\n",
+                DateTime.Now);
+
+            MessageBox.Show(dataTime, "Время и дата в различных форматах");
+
+        }
+    }
+}
---
+++ lab_013/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab_013")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_013")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e0663d4d-6a93-4e87-acd0-6aeb28ed49cd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.7.0949")]
+[assembly: AssemblyFileVersion("1.21.1.7")]
