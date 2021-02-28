b99269624a4d4dcd89ce5802bc08004b477e572d
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-03-01 00:19:55 +0200

    Пример 45. Форматирование Double-переменных в виде таблицы. Вывод таблицы на печать. Поток StringReader

---
+++ lab_045/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_045
+{
+    public partial class Form1 : Form
+    {
+        System.IO.StringReader reader;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Формирование таблицы";
+
+            double[] X = { 5342736.17653, 2345.3333, 234683.853749, 2438454.825368, 3425.72564, 5243.25, 537407.6236, 6354328.9876, 5342.243 };
+
+            double[] Y = { 27488.17, 3806703.356, 22345.72, 54285.34, 2236767.3267, 57038.76, 201722.3, 26434.001, 2164.022 };
+
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Font = new Font("Courier New", 9.0F);
+            textBox1.Text = "КАТАЛОГ КООРДИНАТ\r\n";
+            textBox1.Text += "---------------------------------\r\n";
+            textBox1.Text += "|Пункт|     X      |     Y      |\r\n";
+            textBox1.Text += "---------------------------------\r\n";
+
+            for (int i = 0; i <= 8; i++)
+            {
+                textBox1.Text += string.Format("| {0,3:D} | {1,10:F2} | {2,10:F2} |", i, X[i], Y[i]) + "\r\n";
+            }
+            textBox1.Text += "---------------------------------\r\n";
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                reader = new System.IO.StringReader(textBox1.Text);
+
+                try
+                {
+                    printDocument1.Print();
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ lab_045/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("lab_045")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_045")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("57b1d097-7238-4444-aaf4-80ed42f0e9d3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.5.59.2214")]
+[assembly: AssemblyFileVersion("1.21.2.28")]
