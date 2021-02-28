6a58118b1aba4b8016e3b6f79cdd524b7eaad59a
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-02-28 17:12:44 +0200

    Пример 27. Простой RTF-редактор

---
+++ lab_027/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace lab_027
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьВФорматеRTFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьВФорматеWindows1251ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьВФорматеRTFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 411);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьВФорматеRTFToolStripMenuItem,
+            this.открытьВФорматеWindows1251ToolStripMenuItem,
+            this.сохранитьВФорматеRTFToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьВФорматеRTFToolStripMenuItem
+            // 
+            this.открытьВФорматеRTFToolStripMenuItem.Name = "открытьВФорматеRTFToolStripMenuItem";
+            this.открытьВФорматеRTFToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.открытьВФорматеRTFToolStripMenuItem.Text = "Открыть в формате RTF";
+            // 
+            // открытьВФорматеWindows1251ToolStripMenuItem
+            // 
+            this.открытьВФорматеWindows1251ToolStripMenuItem.Name = "открытьВФорматеWindows1251ToolStripMenuItem";
+            this.открытьВФорматеWindows1251ToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.открытьВФорматеWindows1251ToolStripMenuItem.Text = "Открыть в формате Windows 1251";
+            // 
+            // сохранитьВФорматеRTFToolStripMenuItem
+            // 
+            this.сохранитьВФорматеRTFToolStripMenuItem.Name = "сохранитьВФорматеRTFToolStripMenuItem";
+            this.сохранитьВФорматеRTFToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.сохранитьВФорматеRTFToolStripMenuItem.Text = "Сохранить в формате RTF";
+            this.сохранитьВФорматеRTFToolStripMenuItem.Click += new System.EventHandler(this.сохранитьВФорматеRTFToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(261, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьВФорматеRTFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьВФорматеWindows1251ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьВФорматеRTFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ lab_027/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_027
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            base.Text = "Простой RTF-редактор";
+            richTextBox1.Clear();
+            openFileDialog1.FileName = @"e:\Text2.txt";
+            saveFileDialog1.Filter = "Файлы RTF (*.RTF) |*.RTF";
+
+            this.открытьВФорматеRTFToolStripMenuItem.Click += new System.EventHandler(this.Open);
+            this.открытьВФорматеWindows1251ToolStripMenuItem.Click += new System.EventHandler(this.Open);
+        }
+
+        private void Open(object sender, EventArgs e)
+        {
+            ToolStripMenuItem t = (ToolStripMenuItem)sender;
+            string format = t.Text;
+
+            try
+            {
+                if (format=="Открыть в формате RTF")
+                {
+                    openFileDialog1.Filter = "Файлы RTF (*.RTF) |*.RTF";
+                    openFileDialog1.ShowDialog();
+
+                    if (openFileDialog1.FileName == null) return;
+
+                    richTextBox1.LoadFile(openFileDialog1.FileName);
+                }
+
+                if (format == "Открыть в формате Windows 1251")
+                {
+                    openFileDialog1.Filter = "Текстовые файлы (*.txt) |*.txt";
+                    openFileDialog1.ShowDialog();
+
+                    if (openFileDialog1.FileName == null) return;
+
+                    richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                }
+
+                richTextBox1.Modified = false;
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message + "\nНет такого файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void сохранитьВФорматеRTFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = openFileDialog1.FileName;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) Write();
+        }
+
+        void Write()
+        {
+            try
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+                richTextBox1.Modified = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (richTextBox1.Modified == false) return;
+
+            DialogResult MBox = MessageBox.Show("Текст был изменент. \nСохранить изменения?", "Простой редактор", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+
+            if (MBox == DialogResult.No) return;
+
+            if (MBox == DialogResult.Cancel) e.Cancel = true;
+
+            if (MBox == DialogResult.Yes)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    Write();
+                    return;
+                }
+                else e.Cancel = true;
+            }
+        }
+    }
+}
