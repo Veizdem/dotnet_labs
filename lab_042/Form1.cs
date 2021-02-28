94f53adcccc3b79365e55ea8f24e0c7b1f8878dc
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-02-28 23:07:12 +0200

    Пример 42. Использование таймера Timer

---
+++ lab_042/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_042
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Timer";
+
+            timer1.Interval = 2000;
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Прошло 2 сек");
+
+        }
+    }
+}
