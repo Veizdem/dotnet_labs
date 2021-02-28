4965eb4876f2fa2c657ce51f72c5af72abb4a19b
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-03-01 01:03:47 +0200

    Пример 48. Табличный ввод данных. DataGridView. DataTable. DataSet. Инструмент для создания файла XML

---
+++ lab_048/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_048
+{
+    public partial class Form1 : Form
+    {
+        DataTable dataTable = new DataTable();
+        DataSet dataSet = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            base.Text = "Почти табличный редактор";
+
+            button1.Text = "Запись";
+
+            if (System.IO.File.Exists(@"e:\tabl.xml") == false)
+            {
+                dataGridView1.DataSource = dataTable;
+
+                dataTable.Columns.Add("Имена");
+                dataTable.Columns.Add("Номера телефонов");
+
+                dataSet.Tables.Add(dataTable);
+
+            } else
+            {
+                dataSet.ReadXml(@"e:\tabl.xml");
+
+                string strXML = dataSet.GetXml();
+
+                dataGridView1.DataMember = "Название таблицы";
+                dataGridView1.DataSource = dataSet;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataTable.TableName = "Название таблицы";
+            dataSet.WriteXml(@"e:\tabl.xml");
+
+        }
+    }
+}
---
+++ lab_048/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("lab_048")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_048")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a06b36b1-1ffc-49fc-86e3-de9bb5d5c743")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.59.2302")]
+[assembly: AssemblyFileVersion("1.21.2.28")]
