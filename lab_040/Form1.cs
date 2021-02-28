678d3521e0a79759638f16478ee47b07aa9f2bb6
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-02-28 22:46:56 +0200

    Пример 40. Имитация нажатия комбинации клавиш <Alt>+<PrintScreen>. Вызов функции Microsoft API

---
+++ lab_040/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace lab_040
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        extern static void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Text = "Microsoft API";
+            button2.Text = "SendKeys";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            keybd_event(44, 1, 0, 0);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("%{PRTSC}");
+
+        }
+    }
+}
