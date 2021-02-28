fba08b55fd695c64a7d220bc5d080b21f156fb69
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-02-28 21:22:57 +0200

    Пример 33. Рисование в форме графических примитивов (фигур)

---
+++ lab_033/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_033
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Выбор графического примитива";
+
+            listBox1.Items.AddRange(new Object[] { "Окружность", "Отрезок", "Прямоугольник", "Сектор", "Текст", "Эллипс", "Закрашенный сектор" });
+
+            Font = new Font("Times New Roman", 14);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Graphics graphics = this.CreateGraphics();
+
+            Pen pen = new Pen(Color.Red);
+
+            Brush brush = new SolidBrush(Color.Red);
+
+            graphics.Clear(SystemColors.Control);
+
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    graphics.DrawEllipse(pen, 50, 50, 150, 150);
+                    break;
+                case 1:
+                    graphics.DrawLine(pen, 50, 50, 200, 200);
+                    break;
+                case 2:
+                    graphics.DrawRectangle(pen, 50, 30, 150, 180);
+                    break;
+                case 3:
+                    graphics.DrawPie(pen, 40, 50, 200, 200, 180, 225);
+                    break;
+                case 4:
+                    string s = "Каждый во что-то верит, но\n" +
+                        "жизнь преподносит сюрпризы.";
+                    graphics.DrawString(s, Font, brush, 10, 100);
+                    break;
+                case 5:
+                    graphics.DrawEllipse(pen, 30, 30, 150, 200);
+                    break;
+                case 6:
+                    graphics.FillPie(brush, 20, 50, 150, 150, 0, 45);
+                    break;
+            }
+        }
+    }
+}
---
+++ lab_033/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("lab_033")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_033")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c132b610-3c58-493b-b21d-c7ce280121f5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.4.59.1922")]
+[assembly: AssemblyFileVersion("1.21.2.28")]
