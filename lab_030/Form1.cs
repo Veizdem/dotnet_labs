a39ff905d4cce9dd9c8b33388e770d1713747269
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-02-28 19:31:35 +0200

    Пример 30. Простейший вывод отображения графического файла в форму

---
+++ lab_030/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_030
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            base.Text = "Рисунок";
+            
+            this.Width = 200;
+            this.Height = 200;
+
+            Image img = new Bitmap("e:/img.jpg");
+
+            e.Graphics.DrawImage(img, 5, 5);
+
+        }
+    }
+}
