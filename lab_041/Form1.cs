9b071cfdfad8fdc02bf15edc6dbda3053869bf5a
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-02-28 23:01:29 +0200

    Пример 41. Запись содержимого буфера обмена в BMP-файл

---
+++ lab_041/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_041
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Сохраняю копию БО в ВМР-файл";
+
+            button1.Text = "Сохранить";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IDataObject data = Clipboard.GetDataObject();
+
+            Bitmap bitmap;
+
+            if (data.GetDataPresent(DataFormats.Bitmap) == true)
+            {
+                bitmap = (Bitmap)data.GetData(DataFormats.Bitmap);
+
+                bitmap.Save(@"e:\clip.bmp");
+
+                MessageBox.Show(@"Изображение сохранено из БО в файл e:\clip.bmp", "Успех");
+            } else
+            {
+                MessageBox.Show("В буфере обмена нет данных в формате Bitmap", "Запишите изображение в БО");
+            }
+        }
+    }
+}
