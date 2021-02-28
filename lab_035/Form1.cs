eba24807bf91c454ed8b1033f4e7b1b4faa3cdea
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-02-28 21:34:51 +0200

    Пример 35. Печать графических примитивов

---
+++ lab_035/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_035
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            printDocument1.Print();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Pen pen = new Pen(Color.Red);
+
+            e.Graphics.DrawEllipse(pen, 50, 50, 150, 150);
+        }
+    }
+}
