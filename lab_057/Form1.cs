b872d197e4bbde5bd7ae85543d71fbf65550db22
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-03-01 02:59:04 +0200

    Пример 57. Использование финансовой функции MS Excel

---
+++ lab_057/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_057
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = "Год. ставка в %";
+            label2.Text = "Срок в месяцах";
+            label3.Text = "Размер кредита";
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+            this.Text = "Расчет платежей";
+            button1.Text = "Расчет";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var XL = new Microsoft.Office.Interop.Excel.Application();
+
+                double pay = XL.WorksheetFunction.Pmt((Convert.ToDouble(textBox1.Text)) / 1200, Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+
+                string ss = string.Format("Каждый месяц следует платить {0:$#.##} долларов", Math.Abs(pay));
+
+                MessageBox.Show(ss);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
---
+++ lab_057/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("lab_057")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_057")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0681860b-d18e-4ce0-b9ce-b0142b734dcf")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.60.0058")]
+[assembly: AssemblyFileVersion("1.21.3.1")]
