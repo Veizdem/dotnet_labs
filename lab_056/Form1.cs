3380ff87ea59493329774414428866eb5161a073
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-03-01 02:48:24 +0200

    Пример 56. Обращение к функциям MS Excel из C#

---
+++ lab_056/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_056
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var xl = new Microsoft.Office.Interop.Excel.Application();
+
+            double PI = xl.WorksheetFunction.Pi();
+
+            this.Text = "PI = " + PI;
+        }
+    }
+}
