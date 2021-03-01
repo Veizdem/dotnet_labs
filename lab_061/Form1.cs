7c8aa7599e326d8efafae1148b34ed8b42144e11
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-03-01 03:58:18 +0200

    Пример 61. Вызов MatLab из программы на C#

---
+++ lab_061/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_061
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Вызвать MatLab";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Type typeMatLab = Type.GetTypeFromProgID("Matlab.Application");
+
+            object matLab = Activator.CreateInstance(typeMatLab);
+
+            object[] commands = new object[] { "x = 0:0.1:6.28; y = sin(x).exp(-x); plot(x,y)" };
+
+            object result = typeMatLab.InvokeMember("Execute", System.Reflection.BindingFlags.InvokeMethod, null, matLab, commands);
+
+        }
+    }
+}
