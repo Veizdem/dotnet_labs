45a8e78384de7fa84ca6ff14aa05926989a61183
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-03-02 20:09:13 +0200

    Пример 70. Добавление записей в таблицу базы данных MS Access

---
+++ lab_070/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using oleDB = System.Data.OleDb;
+
+namespace lab_070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new oleDB.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=e:\\new_DB.mdb");
+
+            connection.Open();
+
+            var command = new oleDB.OleDbCommand("INSERT INTO [Phones] (fio, phone) VALUES ('Света-Х', '521-61-41')");
+
+            command.Connection = connection;
+
+            command.ExecuteNonQuery();
+
+            MessageBox.Show("В таблицу 'Phones' добавлена запись");
+            
+            connection.Close();
+        }
+    }
+}
