9f17da9310ccdaff8c0d1ab6632ae03c77a51985
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2021-03-02 15:43:20 +0200

    Пример 68. Создание базы данных MS Access в программном коде

---
+++ lab_068/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_068
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ADOX.Catalog catalog = new ADOX.Catalog();
+
+            try
+            {
+                catalog.Create("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=e:\\new_DB.mdb");
+
+                MessageBox.Show("База данных Е:\\new_DB.mdb успешно создана");
+            }
+            catch (System.Runtime.InteropServices.COMException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                catalog = null;
+            }
+        }
+    }
+}
